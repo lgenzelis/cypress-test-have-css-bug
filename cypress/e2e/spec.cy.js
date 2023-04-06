f073f3668d6a33fc88e0027330e908b53844c2b0
Lucas Genzelis <lucas.genzelis@gmail.com>
2023-04-06 17:20:46 -0300

    test: bug reproduction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
+  it('test fails as expected', () => {
     cy.visit('https://example.cypress.io')
+    // #00bf88 is the color of the banner, so this should fail
+    cy.get('.banner').should('have.css', 'backgroundColor', 'rgb(128, 0, 0)')
+  })
+
+  it('test passes incorrectly', () => {
+    cy.visit('https://example.cypress.io')
+    // Not only does this pass, but it assigns .banner the background color rgb(128, 0, 0)
+    cy.get('.banner').should('have.css', { backgroundColor: 'rgb(128, 0, 0)' } )
   })
 })
